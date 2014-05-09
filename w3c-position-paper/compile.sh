f5a86ceda30507136bbcf81a9cef98aa7600e0fc
Hannes Tschofenig <hannes.tschofenig@gmx.net>
2014-05-09 21:01:17 +0200

    Position Paper for the W3C Web of Things workshop by Michael Koster.

---
+++ w3c-position-paper/compile.sh
@@ -0,0 +1,8 @@
+pdflatex w3c-position-paper.tex
+pdflatex w3c-position-paper.tex
+pdflatex w3c-position-paper.tex
+bibtex w3c-position-paper
+bibtex w3c-position-paper 
+pdflatex w3c-position-paper.tex
+pdflatex w3c-position-paper.tex
+pdflatex w3c-position-paper.tex
