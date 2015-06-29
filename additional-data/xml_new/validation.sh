9cfefc3de21ab1cc23cd255ae348347b90dff3ae
Hannes Tschofenig <hannes.tschofenig@gmx.net>
2015-06-29 21:16:58 +0200

    Examples and schemas from the additional data draft.

---
+++ additional-data/xml_new/validation.sh
@@ -0,0 +1,11 @@
+
+xmllint --noout EmergencyCallData.ProviderInfo.Example.xml --schema ProviderInfo.xsd
+xmllint --noout EmergencyCallData.ServiceInfo.Example.xml --schema ServiceInfo.xsd
+xmllint --noout EmergencyCallData.DeviceInfo.Example.xml --schema DeviceInfo.xsd
+xmllint --noout EmergencyCallData.SubscriberInfo.Example.xml --schema SubscriberInfo.xsd
+xmllint --noout EmergencyCallData.Comment.Example.xml --schema Comment.xsd
+
+xmllint --noout provided-by-Example.xml --schema provided-by.xsd
+xmllint --noout provided-by-Example2.xml --schema provided-by.xsd
+
+
