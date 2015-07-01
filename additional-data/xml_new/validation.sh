d60bb6523c0e93ed734792434492860b8c21066f
Hannes Tschofenig <hannes.tschofenig@gmx.net>
2015-07-01 11:01:17 +0200

    Updated the xmllint validation script to include other examples as well. The PIDF-LO example from Figure 18 is not yet included/working.

--- additional-data/xml_new/validation.sh
+++ additional-data/xml_new/validation.sh
@@ -1,3 +1,4 @@
+# Validation of the XML instance documents from draft-ietf-ecrit-additional-data-30.txt
 
 xmllint --noout EmergencyCallData.ProviderInfo.Example.xml --schema ProviderInfo.xsd
 xmllint --noout EmergencyCallData.ServiceInfo.Example.xml --schema ServiceInfo.xsd
@@ -5,7 +6,13 @@ xmllint --noout EmergencyCallData.DeviceInfo.Example.xml --schema DeviceInfo.xsd
 xmllint --noout EmergencyCallData.SubscriberInfo.Example.xml --schema SubscriberInfo.xsd
 xmllint --noout EmergencyCallData.Comment.Example.xml --schema Comment.xsd
 
-xmllint --noout provided-by-Example.xml --schema provided-by.xsd
-xmllint --noout provided-by-Example2.xml --schema provided-by.xsd
+xmllint --noout Figure16-DeviceInfo.xml --schema DeviceInfo.xsd
+xmllint --noout Figure16-ProviderInfo.xml --schema ProviderInfo.xsd
 
+xmllint --noout Figure17-DataProviderContact.xml --schema ProviderInfo.xsd
+xmllint --noout Figure17-DeviceInfo.xml --schema DeviceInfo.xsd
+xmllint --noout Figure17-ProviderInfo-Client.xml --schema ProviderInfo.xsd
+xmllint --noout Figure17-ProviderInfo-ServiceProvider.xml --schema ProviderInfo.xsd
+xmllint --noout Figure17-ServiceInfo.xml --schema ServiceInfo.xsd
 
+# xmllint --noout Figure18-PIDF-LO.xml --schema pidf.xsd geopriv10.xsd basicPolicy.xsd data-model.xsd
